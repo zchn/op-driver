21dec007e67e11934c8b75ac7cb8ac2d15c74b97
Kevin Kz <k@oplabs.co>
2024-09-22 21:58:48 -0400

    Commit.

--- src/OpDriver.sol
+++ src/OpDriver.sol
@@ -4,9 +4,8 @@ pragma solidity ^0.8.15;
 import {L1CrossDomainMessenger} from "@eth-optimism-bedrock/src/L1/L1CrossDomainMessenger.sol";
 
 contract OpDriver {
-    // OP Sepolia
-    const address l1CrossDomainMessenger = 0x58Cc85b8D04EA49cC6DBd3CbFFd00B4B8D6cb3ef;
     function run() public {
-        number++;
+        // OP Sepolia
+        L1CrossDomainMessenger l1CrossDomainMessenger = L1CrossDomainMessenger(0x58Cc85b8D04EA49cC6DBd3CbFFd00B4B8D6cb3ef);
     }
 }
