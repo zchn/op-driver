aa2e22938474fde0e2ce12ed03ff6f39e6c341b0
Kevin Kz <k@oplabs.co>
2024-09-22 21:43:00 -0400

    Add partial OpDriver.

---
+++ src/OpDriver.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract OpDriver {
+    // OP Sepolia
+    const address l1CrossDomainMessenger = 0x58Cc85b8D04EA49cC6DBd3CbFFd00B4B8D6cb3ef;
+    function run() public {
+        number++;
+    }
+}
