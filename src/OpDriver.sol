f187e4f9c3fac012afef35f28f2c4eaebb2f315a
Kevin Kz <k@oplabs.co>
2024-09-22 21:46:52 -0400

    Add remappings and import L1Crossdomainmessenger.

--- src/OpDriver.sol
+++ src/OpDriver.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {L1CrossDomainMessenger} from "@eth-optimism-bedrock/src/L1/L1CrossDomainMessenger.sol";
+
 contract OpDriver {
     // OP Sepolia
     const address l1CrossDomainMessenger = 0x58Cc85b8D04EA49cC6DBd3CbFFd00B4B8D6cb3ef;
